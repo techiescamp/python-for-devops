8d839842c18d44d6d19dd55a63f0fdf2158a4be7
scriptcamp <scriptcamp.io@gmail.com>
2022-08-20 16:30:22 +0530

    Scripts

---
+++ json/parse-json.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+# Script to create absolute path of the JSON file.
+
+script_dir = os.path.dirname(__file__)
+print("The Script is located at:" + script_dir )
+script_absolute_path = os.path.join(script_dir, 'example.json')
+print("The Script Path is:" + script_absolute_path)
+
+# Script to parse JSON
+
+json = json.loads(open(script_absolute_path).read())
+value = json['name']
+print(value)
+
+# Loop through JSON
+
+for key in json:
+    value = json[key]
+    print("The key and value are ({}) = ({})".format(key, value))
